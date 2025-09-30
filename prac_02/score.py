885e134340794827c053fcf839ed194e51abb456
braithseidenkranz <braith.seidenkranz@my.jcu.edu.au>
2025-09-30 18:17:03 +1000

    Refactor score program to use functions

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,13 +2,27 @@
 CP1404/CP5632 - Practical
 Program to determine score status
 """
+import random
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+def main():
+    score = float(input("Enter score: "))
+    result = get_score_result(score)
+    print(result)
+
+    random_score = random.randint(0, 100)
+    print(f"Random score is {random_score}")
+    random_score_result = get_score_result(random_score)
+    print(f"{random_score_result}")
+
+
+def get_score_result(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+main()
