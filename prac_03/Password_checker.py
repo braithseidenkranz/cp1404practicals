9fc490aa2b6b5439bc8408f2c3cad1c5914252ab
braithseidenkranz <braith.seidenkranz@my.jcu.edu.au>
2025-10-05 16:33:46 +1000

    Finished the TODO comments in is_valid_password function

--- prac_03/Password_checker.py
+++ prac_03/Password_checker.py
@@ -3,11 +3,10 @@ CP1404/CP5632 - Practical
 Password checker "skeleton" code to help you get started
 """
 
-MIN_LENGTH = 2
-MAX_LENGTH = 6
-IS_SPECIAL_CHARACTER_REQUIRED = False
+MIN_LENGTH = 5
+MAX_LENGTH = 15
+IS_SPECIAL_CHARACTER_REQUIRED = True
 SPECIAL_CHARACTERS = "!@#$%^&*()_-=+`~,./'[]<>?{}|\\"
-
 
 def main():
     """Program to get and check a user's password."""
@@ -24,26 +23,29 @@ def main():
         password = input("> ")
     print(f"Your {len(password)}-character password is valid: {password}")
 
-
 def is_valid_password(password):
     """Determine if the provided password is valid."""
-    # TODO: if length is wrong, return False
-
+    if len(password) < MIN_LENGTH or len(password) > MAX_LENGTH:
+        return False
     number_of_lower = 0
     number_of_upper = 0
     number_of_digit = 0
     number_of_special = 0
     for character in password:
-        # TODO: count each kind of character (use str methods like isdigit)
-        pass
-
-    # TODO: if any of the 'normal' counts are zero, return False
-
-    # TODO: if special characters are required, then check the count of those
-    # and return False if it's zero
+        if character.islower():
+            number_of_lower += 1
+        if character.isupper():
+            number_of_upper += 1
+        if character.isdigit():
+            number_of_digit += 1
+        if character in SPECIAL_CHARACTERS:
+            number_of_special += 1
+    if number_of_lower == 0 or number_of_upper == 0 or number_of_digit == 0:
+        return False
+    if IS_SPECIAL_CHARACTER_REQUIRED and number_of_special == 0:
+        return False
 
     # if we get here (without returning False), then the password must be valid
     return True
-
 
 main()
