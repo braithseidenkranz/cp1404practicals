18415df09a4b68ce81e9deca36ec1b31548e6ba5
braithseidenkranz <braith.seidenkranz@my.jcu.edu.au>
2025-10-12 12:50:58 +1000

    Add program to get 5 numbers and display summary

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,14 @@
+
+def main():
+    """Ask user for 5 numbers, store then, and display results"""
+    numbers = []
+    for i in range(5):
+        number = float(input("Enter number: "))
+        numbers.append(number)
+    print(f"The first number is {numbers[0]}")
+    print(f"The last number is {numbers[-1]}")
+    print(f"The smallest number is {min(numbers)}")
+    print(f"The largest number is {max(numbers)}")
+    print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
+
+main()
