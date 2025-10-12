6a49e9372bc5a0007edf41941a5fe3f083df8e39
braithseidenkranz <braith.seidenkranz@my.jcu.edu.au>
2025-10-12 12:15:36 +1000

    Added display_subject_details function and renamed variable 'data'

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,23 +7,29 @@ FILENAME = "subject_data.txt"
 
 
 def main():
-    data = load_data(FILENAME)
-    print(data)
+    subject_records = load_subject_records(FILENAME)
+    print(subject_records)
+    display_subject_details(subject_records)
 
 
-def load_data(filename=FILENAME):
+def load_subject_records(filename=FILENAME):
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(filename)
+    subject_records = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        subject_records.append(parts)
     input_file.close()
+    return subject_records
 
-
+def display_subject_details(subject_details):
+    """Display formatted subject details from the nested list"""
+    print("\nSubject details:")
+    for record in subject_details:
+        subject_code = record[0]
+        lecturer = record[1]
+        number_of_students = record[2]
+        print(f"{subject_code} is taught by {lecturer} and has {number_of_students} students.\n")
 main()
