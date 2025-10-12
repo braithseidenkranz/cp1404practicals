62287c3eafd06bf22d4be035e3a6a365747877d1
braithseidenkranz <braith.seidenkranz@my.jcu.edu.au>
2025-10-12 13:05:35 +1000

    Add check_username function and refactored the part 1

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,14 +1,35 @@
 
 def main():
-    """Ask user for 5 numbers, store then, and display results"""
+    numbers = get_numbers()
+    print_numbers_results(numbers)
+
+    check_username() # part 2
+
+def get_numbers():
+    """Ask user for 5 numbers and store them"""
     numbers = []
-    for i in range(5):
+    for _ in range(5):
         number = float(input("Enter number: "))
         numbers.append(number)
+    return numbers
+
+def print_numbers_results(numbers):
+    """Print a summary of the numbers given"""
     print(f"The first number is {numbers[0]}")
     print(f"The last number is {numbers[-1]}")
     print(f"The smallest number is {min(numbers)}")
     print(f"The largest number is {max(numbers)}")
     print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
+
+def check_username():
+    """Checks if the username entered is in the given list"""
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface',
+                 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer',
+                 'bob']
+    username = input("Enter username: ")
+    if username in usernames:
+        print("Access granted")
+    else:
+        print("Access denied")
 
 main()
