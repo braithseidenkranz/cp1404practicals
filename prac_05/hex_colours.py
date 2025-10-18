a19f863b4afd98397655ba4b24241eba8d226f37
braithseidenkranz <braith.seidenkranz@my.jcu.edu.au>
2025-10-18 14:14:27 +1000

    Complete hex colours exercise

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,16 @@
+COLOUR_TO_CODE = {"Absolute Zero": "#0048ba", "Acid Green": "#b0bf1a", "Alice Blue": "#f0f8ff", "Alizarin crimson": "#e32636",
+                "Amaranth": "#e52b50", "Amber": "#ffbf00", "Antique White": "#faebd7", "Amethyst": "#9966cc", "Apricot": "#fbceb1", "Aqua": "#00ffff"}
+print(COLOUR_TO_CODE)
+
+max_key_length = max(len(key) for key in COLOUR_TO_CODE)
+for colour, code in COLOUR_TO_CODE.items():
+    print(f"{colour:{max_key_length}} is {code}")
+
+
+colour = input("Enter colour: ").title()
+while colour != "":
+    try:
+        print(colour, "is", COLOUR_TO_CODE[colour])
+    except KeyError:
+        print("Invalid colour")
+    colour = input("Enter colour: ").title()
