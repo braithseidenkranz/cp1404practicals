49e6bb410beb21e536024b639f8958415414a633
braithseidenkranz <braith.seidenkranz@my.jcu.edu.au>
2025-11-02 17:22:01 +1000

    Rename 'key' to 'colour'

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -2,9 +2,9 @@ COLOUR_TO_CODE = {"Absolute Zero": "#0048ba", "Acid Green": "#b0bf1a", "Alice Bl
                 "Amaranth": "#e52b50", "Amber": "#ffbf00", "Antique White": "#faebd7", "Amethyst": "#9966cc", "Apricot": "#fbceb1", "Aqua": "#00ffff"}
 print(COLOUR_TO_CODE)
 
-max_key_length = max(len(key) for key in COLOUR_TO_CODE)
+max_colour_length = max(len(colour) for colour in COLOUR_TO_CODE)
 for colour, code in COLOUR_TO_CODE.items():
-    print(f"{colour:{max_key_length}} is {code}")
+    print(f"{colour:{max_colour_length}} is {code}")
 
 
 colour = input("Enter colour: ").title()
