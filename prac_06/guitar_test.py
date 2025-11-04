922312430540b9f9dbf7817cfc552585b75491a2
braithseidenkranz <braith.seidenkranz@my.jcu.edu.au>
2025-11-04 14:49:54 +1000

    Add guitar_test.py

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -1,33 +1,9 @@
-"""
-Estimate: 20 mins
-Actual: 18 mins
-"""
+from prac_06.guitars import Guitar
 
-from prac_06.guitar import Guitar
+guitar_1 = (Guitar("Gibson L-5 CES", 1922, 16035.40))
+guitar_2 = (Guitar("Line 6 JTV-59", 2010, 1512.9))
 
-def main():
-    print("My Guitars!")
-    guitars = []
-
-    name = input("Name: ").strip
-    while name != "":
-        year = int(input("Year: "))
-        cost = float(input("Cost: "))
-
-        guitar = Guitar(name, year, cost)
-        guitars.append(guitar)
-
-        print(f"{guitar} added.\n")
-
-        name = input("Name: ").strip
-
-    #guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
-    #guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
-
-    print("These are my guitars:")
-    for i, guitar in enumerate(guitars, 1):
-        vintage_string = " (vintage)" if guitar.is_vintage() else ""
-        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
-
-if __name__ == "__main__":
-    main()
+print(f"Gibson L-5 CES get_age() - Expected 103 got {guitar_1.get_age()}")
+print(f"Line 6 JTV-59 get_age() - Expected 15 got {guitar_2.get_age()}")
+print(f"Gibson L-5 CES is_vintage() - Expected True got {guitar_1.is_vintage()}")
+print(f"Line 6 JTV-59 is_vintage() - Expected False got {guitar_2.is_vintage()}")
