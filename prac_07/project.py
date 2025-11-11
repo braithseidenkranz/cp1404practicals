6bfc38f286d7623e080ca93bab2084e635bc1723
braithseidenkranz <braith.seidenkranz@my.jcu.edu.au>
2025-11-11 11:26:59 +1000

    Add docstrings

--- prac_07/project.py
+++ prac_07/project.py
@@ -3,9 +3,9 @@ from datetime import date
 
 
 class Project:
-
+    """Represents information about a project"""
     def __init__(self, name, start_date, priority, cost, completion):
-
+        """Initialises a project from the given information"""
         self.name = name
         self.start_date = start_date
         self.priority = priority
@@ -17,7 +17,9 @@ class Project:
         return self.priority < other.priority
 
     def __str__(self):
+        """Returns a string representation of the project"""
         return f"{self.name}, start: {self.start_date}, priority {self.priority}, estimate: ${self.cost}, completion {self.completion}%"
 
     def is_complete(self):
+        """Returns true if the project is complete"""
         return self.completion == 100
