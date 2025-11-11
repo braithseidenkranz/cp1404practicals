ae11400338c7852d647e9f8b6624d1356260b2bd
braithseidenkranz <braith.seidenkranz@my.jcu.edu.au>
2025-11-11 11:18:03 +1000

    Complete Project class for project management program

---
+++ prac_07/project.py
@@ -0,0 +1,23 @@
+
+from datetime import date
+
+
+class Project:
+
+    def __init__(self, name, start_date, priority, cost, completion):
+
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost = cost
+        self.completion = completion
+
+
+    def __lt__(self, other):
+        return self.priority < other.priority
+
+    def __str__(self):
+        return f"{self.name}, start: {self.start_date}, priority {self.priority}, estimate: ${self.cost}, completion {self.completion}%"
+
+    def is_complete(self):
+        return self.completion == 100
