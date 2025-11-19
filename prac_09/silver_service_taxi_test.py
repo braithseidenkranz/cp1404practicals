d5870639d28f2b08a54907a738558302a70cb81d
braithseidenkranz <braith.seidenkranz@my.jcu.edu.au>
2025-11-19 17:56:29 +1000

    Complete SilverServiceTaxi exercise

---
+++ prac_09/silver_service_taxi_test.py
@@ -0,0 +1,12 @@
+from silver_service_taxi import SilverServiceTaxi
+
+
+my_car = SilverServiceTaxi("Prius", 200, fanciness=2)
+my_car.start_fare()
+my_car.drive(18)
+fare = my_car.get_fare()
+print(my_car)
+print(f"Fare for 18 km trip: ${fare}")
+assert fare - 48.78 == 0.0, "Fare should be $48.78"
+
+
