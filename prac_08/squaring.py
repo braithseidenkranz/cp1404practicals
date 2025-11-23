f2f9476080d237aa9f03cb1da06b3f6bf80ba462
braithseidenkranz <braith.seidenkranz@my.jcu.edu.au>
2025-11-23 11:54:12 +1000

    Fix docstring

--- prac_08/squaring.py
+++ prac_08/squaring.py
@@ -1,15 +1,13 @@
 """
-CP1404/CP5632 Practical
+CP1404 Practical
 Kivy GUI program to square a number
-Lindsay Ward, IT@JCU
-Started 13/10/2015
 """
 
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
 
-__author__ = 'Lindsay Ward'
+__author__ = 'Braith Seidenkranz'
 
 
 class SquareNumberApp(App):
